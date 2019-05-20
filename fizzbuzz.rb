9acf940db765d860d1518cbaf48569dd6d354815
Ryan Bravo <rrbravo2@gmail.com>
2019-05-20 04:49:29 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,10 @@
 def fizzbuzz(number)
- 
+ if number % 3 == 0 && number % 5 == 0
+   "FizzBuzz"
+ elsif number % 3 == 0 
+ "Fizz"
+ elsif number % 5 == 0
+ "Buzz"
 end
 
 # Don't forget! This file needs to be 'required' in its spec file
